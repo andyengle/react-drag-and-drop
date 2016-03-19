368d2ddad9cf21fa6097a24611f2f06b3e57c8fc
Andy Engle <aengle@exacttarget.com>
2016-03-19 12:38:36 -0400

    First commit

---
+++ src/Knight.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Knight extends Component {
+  render() {
+    return (
+      <img style={{ padding: '3px 0 0 3px' }} src="http://localhost:4002/will-and-daddy.png" />
+    );
+  }
+}
